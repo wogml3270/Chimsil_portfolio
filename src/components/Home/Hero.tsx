ea032e0411bfcc2be812e38dfdfc6b41f73f0cb4
wogml3270 <wogml3270@naver.com>
2023-06-27 15:21:45 +0900

    pjh

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -2,33 +2,29 @@ import Link from 'next/link'
 import Animation from './Animation'
 import styles from './Home.module.scss'
 
-type Props = {}
-
-const Hero = (props: Props) => {
+const Hero = () => {
   return (
-    <>
-      <section className={styles.hero}>
-        <div className={styles.container}>
-          <div className={styles.title}>
-            <h1>
-              Hello, I'm{" "}
-              <span>
-                chimsil
-              </span>
-            </h1>
-            <p>
-              이 사이트는 프론트엔드 개발자 박재희의 개인 포트폴리오입니다.
-            </p>
-            <button>
-              <Link href="/project">projects view</Link>
-            </button>
-          </div>
-          <div className={styles.animation}>
-            <Animation />
-          </div>
+    <section className={styles.hero}>
+      <div className={styles.container}>
+        <div className={styles.title}>
+          <h1>
+            Hello, I'm{" "}
+            <span>
+              chimsil
+            </span>
+          </h1>
+          <p>
+            이 사이트는 프론트엔드 개발자 박재희의 개인 포트폴리오입니다.
+          </p>
+          <button>
+            <Link href="/project">projects view</Link>
+          </button>
         </div>
-      </section>
-    </>
+        <div className={styles.animation}>
+          <Animation />
+        </div>
+      </div>
+    </section>
   )
 }
 
