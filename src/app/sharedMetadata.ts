1f830f780f9bf7a015c8ffc091f00ddc14e4c367
wogml3270 <wogml3270@naver.com>
2023-06-30 16:40:07 +0900

    refactor: logo component refactoring

---
+++ src/app/sharedMetadata.ts
@@ -0,0 +1,13 @@
+import { Metadata } from 'next';
+
+interface Props {
+  title: string;
+  description: string;
+}
+
+export const getMetadata = ({title, description}: Props): Metadata => {
+  return {
+    title,
+    description,
+  }
+}
