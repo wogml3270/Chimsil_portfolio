a6bf101253b1477a8337127c95fb267f7f44f520
wogml3270 <wogml3270@naver.com>
2023-07-10 19:18:31 +0900

    kakao login 구현중

---
+++ src/hooks/useKakaoLogin.ts
@@ -0,0 +1,52 @@
+import { useEffect } from 'react';
+
+interface KakaoLoginOptions {
+  onSuccess: (response: any) => void;
+  onFailed?: (error: any) => void;
+}
+
+const useKakaoLogin = (options: KakaoLoginOptions) => {
+  const { onSuccess, onFailed } = options;
+
+  useEffect(() => {
+    const handleKakaoLogin = () => {
+      if (window.Kakao) {
+        window.Kakao.Auth.login({
+          success: onSuccess,
+          fail: onFailed,
+        });
+      } else {
+        console.error('Kakao JavaScript SDK is not initialized.');
+      }
+    };
+
+    const initializeKakaoSDK = () => {
+      if (!window.Kakao) {
+        console.error('Kakao JavaScript SDK is not loaded.');
+        return;
+      }
+
+      if (!window.Kakao.isInitialized()) {
+        console.error('Kakao JavaScript SDK is not initialized.');
+        return;
+      }
+
+      // Add event listener to trigger Kakao login
+      const loginButton = document.querySelector('#kakaoLoginBtn');
+      if (loginButton) {
+        loginButton.addEventListener('click', handleKakaoLogin);
+      }
+    };
+
+    initializeKakaoSDK();
+
+    return () => {
+      const loginButton = document.querySelector('#kakaoLoginBtn');
+      if (loginButton) {
+        loginButton.removeEventListener('click', handleKakaoLogin);
+      }
+    };
+  }, [onSuccess, onFailed]);
+};
+
+export default useKakaoLogin;
